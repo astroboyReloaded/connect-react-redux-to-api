826b31b34615d5dc47e4e6a9c70de91d8915ffc6
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-04-12 13:04:19 -0600

    Create fetchUsers action creator

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,10 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {
+  createSlice,
+  createAsyncThunk,
+  isRejectedWithValue,
+} from '@reduxjs/toolkit';
+
+const USERS_URL = 'https://randomuser.me/api/?results=5';
 
 const initialState = {
   users: [],
   isLoading: false,
   error: undefined,
 };
+
+export const fetchUsers = createAsyncThunk(
+  'users/fetchUsers',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await fetch(USERS_URL);
+      const fetchedData = await response.json();
+      return fetchedData.response;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
 
 const usersSlice = createSlice({
   name: 'users',
