e4bbd403882f010e687a22036323b7a20bedbfa1
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-04-12 13:19:05 -0600

    Modify extraReducers

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -33,7 +33,20 @@ const usersSlice = createSlice({
       console.log(state.users);
     },
   },
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export const { users } = usersSlice.actions;
